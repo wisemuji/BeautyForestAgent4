bcdab97f0dd327388e41e8deefc1247bbfcdc1ac
wisemuji <wisemuji@gmail.com>
2019-10-04 00:02:35 +0900

    #3 코드 구현 - 3604 김수현

--- BeautyForestAgent/FormMemo.Designer.cs
+++ BeautyForestAgent/FormMemo.Designer.cs
@@ -142,6 +142,7 @@
             this.btnLoadFileSelect.TabIndex = 2;
             this.btnLoadFileSelect.Text = "파일 선택";
             this.btnLoadFileSelect.UseVisualStyleBackColor = true;
+            this.btnLoadFileSelect.Click += new System.EventHandler(this.btnLoadFileSelect_Click);
             // 
             // btnLoadFile
             // 
@@ -151,6 +152,7 @@
             this.btnLoadFile.TabIndex = 3;
             this.btnLoadFile.Text = "불러오기";
             this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
             // 
             // txtSaveText
             // 
@@ -177,6 +179,7 @@
             this.btnSaveFileSelect.TabIndex = 2;
             this.btnSaveFileSelect.Text = "파일 선택";
             this.btnSaveFileSelect.UseVisualStyleBackColor = true;
+            this.btnSaveFileSelect.Click += new System.EventHandler(this.btnSaveFileSelect_Click);
             // 
             // btnSaveFile
             // 
@@ -186,6 +189,7 @@
             this.btnSaveFile.TabIndex = 3;
             this.btnSaveFile.Text = "저장하기";
             this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
             // 
             // FormMemo
             // 
